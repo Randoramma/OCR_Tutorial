5674791175af48b34b78964656b96a8d9c2897b0
Randy McLain <Randoramma@me.com>
2015-07-06 17:45:31 -0700

    imagePicker added.

--- LoveInASnap/LoveInASnap/ViewController.swift
+++ LoveInASnap/LoveInASnap/ViewController.swift
@@ -37,17 +37,31 @@ class ViewController: UIViewController, UITextViewDelegate, UINavigationControll
     let imagePickerActionSheet = UIAlertController (title: "Upload Photo", message: nil, preferredStyle: .ActionSheet)
     
     if UIImagePickerController .isSourceTypeAvailable(.Camera) {
-      let cameraButton = UIAlertAction(title: "Take Photo", style: .Default, handler: { (alert) -> Void in
+      let cameraButton = UIAlertAction(title: "Take Photo", style: .Default) { (alert) -> Void in
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .Camera
         self.presentViewController(imagePicker, animated: true, completion: nil)
-      })
-      
-      
+      }
+      imagePickerActionSheet.addAction(cameraButton)
     }
     
-  }
+    let libraryButton = UIAlertAction(title: "Choose Existing", style: .Default) { (alert) -> Void in
+      let imagePicker = UIImagePickerController()
+      imagePicker.delegate = self
+      imagePicker.sourceType = .PhotoLibrary
+      self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    imagePickerActionSheet.addAction(libraryButton)
+    
+    let cancelButton = UIAlertAction (title: "Cancel", style: .Cancel) { (alert) -> Void in
+    }
+    imagePickerActionSheet.addAction(cancelButton)
+    
+    presentViewController(imagePickerActionSheet, animated: true, completion: nil)
+    
+    
+  } // takePhoto
   
   @IBAction func swapText(sender: AnyObject) {
     
